6c1866e41a4d95efcea76f0b725ce4811477d4e5
Striker2783 <owenxingli@gmail.com>
2022-12-24 11:33:06 -0500

    Fired bindable in default when Cooldown passes

--- src/server/Moves/init.lua
+++ src/server/Moves/init.lua
@@ -1,10 +1,12 @@
+local RS = game:GetService("ReplicatedStorage")
 --Module that has the stats for moves
 local default: MoveFunctions = {
 	COOLDOWN = 1, --Cooldown in seconds
 	ACTIVE_TIME = 1, --Active time in seconds
-	deactivate = function()
-	end,
-	activate = function()
+	deactivate = function(self: MoveFunctions, player: Player) end,
+	activate = function(self: MoveFunctions, player: Player)
+		task.wait(self.COOLDOWN)
+		RS.Events.Deactivate:Fire(player, script.Name)
 	end,
 }
 default.__index = default
